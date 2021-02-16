e924a036825ca9ebb50498075a8f3f35d4ceb74b
wasiq <abdul.wasiq@tcs.com.pk>
2021-02-16 19:05:56 +0500

    tracking ui screen updated code

---
+++ tcs_one_app/Views/TableView/TrackerHeaderTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrackerHeaderTableCell.swift
+//  tcs_one_app
+//
+//  Created by TCS on 16/02/2021.
+//  Copyright © 2021 Personal. All rights reserved.
+//
+
+import UIKit
+
+class TrackerHeaderTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
