a881995e69f92acaf2803be6a45e500bce845913
wasiq <abdul.wasiq@tcs.com.pk>
2021-09-08 16:04:20 +0500

    detail screen is in progress

--- tcs_one_app/Controller/Home Storyboard/MIS /MISDetailViewController.swift
+++ tcs_one_app/Controller/Home Storyboard/MIS /MISDetailViewController.swift
@@ -15,9 +15,17 @@ class MISDetailViewController: BaseViewController {
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var collectionView: UICollectionView!
     var mis_budget_setup: tbl_mis_budget_setup?
     var mis_popup_mnth: MISPopupMonth?
     var mis_popop_year: MISPopupYear?
+    
+    var monthName: String = ""
+    var year: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.makeTopCornersRounded(roundView: self.mainView)
@@ -25,6 +33,24 @@ class MISDetailViewController: BaseViewController {
         if let product = mis_budget_setup?.product {
             headingLabel.text = product
         }
+        tableView.register(UINib(nibName: MISDetailTableCell.description(), bundle: nil), forCellReuseIdentifier: MISDetailTableCell.description())
+        tableView.register(UINib(nibName: "MISHeaderCell", bundle: nil), forHeaderFooterViewReuseIdentifier: "MISHeaderCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 30
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.tableViewHeightConstraint.constant = (30 * 5) + 40
+        }
+        
+        let df = DateFormatter()
+        df.dateFormat = "MMMM"
+        monthName = df.string(from: Date())
+        df.dateFormat = "yyyy"
+        year = df.string(from: Date())
+        
+        self.monthLabel.text = monthName
+        self.yearLabel.text = year
     }
     
     @IBAction func selectionBtnTapped(_ sender: UIButton) {
@@ -47,7 +73,50 @@ class MISDetailViewController: BaseViewController {
     }
 }
 
-
+extension MISDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MISDetailTableCell.description()) as? MISDetailTableCell else {
+            fatalError()
+        }
+        cell.dateView.isHidden = true
+        cell.shipmentBookedLabel.text = "Shipment Booked"
+        cell.weightLabel.text = "Budgeted"
+        cell.qsrLabel.text = "Actual"
+        cell.dsrLabel.text = "Variance"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerCell = tableView.dequeueReusableHeaderFooterView(withIdentifier: "MISHeaderCell") as! MISHeaderCell
+        if let dateView = headerCell.viewWithTag(1) as? CustomView {
+            dateView.isHidden = true
+        }
+        if let shipmentLabel = headerCell.viewWithTag(20) as? UILabel {
+            shipmentLabel.text = ""
+        }
+        if let budgeted = headerCell.viewWithTag(30) as? UILabel {
+            budgeted.text = "Budgeted"
+        }
+        if let actual = headerCell.viewWithTag(40) as? UILabel {
+            actual.text = "Actual"
+        }
+        if let variance = headerCell.viewWithTag(50) as? UILabel {
+            variance.text = "Variance"
+        }
+        
+        return headerCell
+    }
+}
 extension MISDetailViewController: MISDelegate {
     func updateListing(region_date: tbl_mis_region_data) {}
     
@@ -58,7 +127,5 @@ extension MISDetailViewController: MISDelegate {
     func updateYearr(year: MISPopupYear) {
         self.yearLabel.text = year.yearr
     }
-    
-    
 }
 
