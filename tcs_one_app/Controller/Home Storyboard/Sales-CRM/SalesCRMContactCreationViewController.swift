fafc7e0522dabeda4df3dac5d393afa29603fac7
Abdul Wasiq <abdul.wasiq@tcs.com.pk>
2022-01-20 18:16:36 +0500

    Contact Creation screen initialised.

---
+++ tcs_one_app/Controller/Home Storyboard/Sales-CRM/SalesCRMContactCreationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SalesCRMContactCreationViewController.swift
+//  tcs_one_app
+//
+//  Created by Wasiq Saleem on 20/01/2022.
+//  Copyright © 2022 Personal. All rights reserved.
+//
+
+import UIKit
+
+class SalesCRMContactCreationViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sales CRM"
+    }
+}
